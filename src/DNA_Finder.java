a83d1e121deff52b0469fea9db644e30e73d6ac9
KJ <tongkj2000@gmail.com>
2022-01-26 12:08:24 +0800

    Initial Commmit

---
+++ src/DNA_Finder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class DNA_Finder {
+    public static void main(String[] args) {
+        String input_array;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter DNA sequence");
+        input_array = input.nextLine();
+        Pattern gtc = Pattern.compile("(GTC)");
+        Matcher matcher = gtc.matcher(input_array);
+        long count = matcher.results().count();
+        System.out.format("Number of GTC sequences is %d.\n", count);
+        if (count < 10){
+            System.out.println("Patient is at lower risk of infection.");
+        }
+        else{
+            System.out.println("Patient is at higher risk of infection.");
+        }
+    }
+}
